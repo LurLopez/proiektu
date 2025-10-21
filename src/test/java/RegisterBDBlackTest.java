73fea562c4ab6fcdf470af16a66a329c7be3a2e7
ehu <ehu@158.227.124.14>
2025-10-21 11:19:13 +0200

    proba 2.0

---
+++ src/test/java/RegisterBDBlackTest.java
@@ -0,0 +1,207 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import dataAccess.DataAccess;
+import domain.Traveler;
+import domain.User;
+import testOperations.TestDataAccess;
+
+public class RegisterBDBlackTest {
+
+	static DataAccess sut=new DataAccess();
+	static TestDataAccess testDA=new TestDataAccess();
+	
+	
+	@Test
+	public void test_1() {
+		
+		String izena = null;
+		String pasahitza = "pasahitza_1";
+		String mota = "Traveler";
+		String mail = "ander@gmail";
+		
+		
+		
+		try {
+			sut.open();
+			User j = sut.register(izena, pasahitza, mota, mail);
+			sut.close();
+			
+			if(j != null) {
+				sut.open();
+				sut.erabiltzaileaEzabatu(j);
+				sut.close();
+				fail();
+			}else {
+				
+				assertTrue(true);
+			}
+		}
+		finally {
+
+			
+			
+		}
+	}
+	
+	@Test
+	public void test_2() {
+		
+		String izena = "Ander2";
+		String pasahitza = null;
+		String mota = "Traveler";
+		String mail = "ander@gmail";
+		
+		User u = new Traveler(izena,pasahitza,mail);
+		try {
+			sut.open();
+			User j = sut.register(izena, pasahitza, mota, mail);
+			sut.close();
+			
+			if(j != null) {
+				sut.open();
+				sut.erabiltzaileaEzabatu(j);
+				sut.close();
+				fail();
+				
+			}else {
+				assertTrue(true);
+			}
+		}
+		finally {
+
+		
+		}
+	}
+	
+	@Test
+	public void test_3() {
+		
+		String izena = "Ander3";
+		String pasahitza = "pasahitza_1";
+		String mota = null;
+		String mail = "ander@gmail";
+		
+		
+		try {
+			sut.open();
+			User j = sut.register(izena, pasahitza, mota, mail);
+			sut.close();
+			
+			if(j != null) {
+				sut.open();
+				sut.erabiltzaileaEzabatu(j);
+				sut.close();
+				fail();
+			}else {
+				
+				assertTrue(true);
+			}
+		}
+		finally {
+
+	
+			
+		}
+	}
+	
+	@Test
+	public void test_4() {
+		
+		String izena = "Ander4";
+		String pasahitza = "pasahitza_1";
+		String mota = "Traveler";
+		String mail = null;
+		
+		
+		try {
+			sut.open();
+			User j = sut.register(izena, pasahitza, mota, mail);
+			sut.close();
+			
+			if(j != null) {
+				sut.open();
+				sut.erabiltzaileaEzabatu(j);
+				sut.close();
+				fail();
+				
+			}else {
+				
+				assertTrue(true);
+			}
+		}
+		finally {
+
+			
+			
+		}
+	}
+	
+	@Test
+	public void test_5() {
+		
+		String izena = "Ander5";
+		String pasahitza = "pasahitza_1";
+		String mota = "Traveler";
+		String mail = "ander@gmail";
+		
+		User u = new Traveler(izena,pasahitza,mail);
+		
+		
+		try {
+			sut.open();
+			User erabiltzailea = sut.register(izena, pasahitza, mota, mail);
+			User j = sut.register(izena, pasahitza, mota, mail);
+			sut.close();
+			
+			if(j != null) {
+				fail();
+				sut.open();
+				sut.erabiltzaileaEzabatu(u);
+				sut.close();
+			}else {
+				assertTrue(true);
+			}
+		}
+		finally {
+
+			
+			
+		}
+	}
+	
+	@Test
+	public void test_6() {
+		
+		String izena = "Ander6";
+		String pasahitza = "pasahitza_1";
+		String mota = "Traveler";
+		String mail = "ander@gmail";
+		
+		
+		
+		
+		try {
+			sut.open();
+			User j = sut.register(izena, pasahitza, mota, mail);
+			sut.close();
+			
+			if(j != null) {
+				sut.open();
+				sut.erabiltzaileaEzabatu(j);
+				sut.close();
+				assertTrue(true);
+			}else {
+				fail();
+			}
+		}
+		finally {
+
+			
+			
+		}
+	}
+	
+
+}
---
+++ src/test/java/RegisterBDWhiteTest.java
@@ -0,0 +1,176 @@
+import static org.junit.Assert.*;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import org.junit.Test;
+
+import dataAccess.DataAccess;
+import domain.Driver;
+import domain.Traveler;
+import domain.User;
+import testOperations.TestDataAccess;
+
+public class RegisterBDWhiteTest {
+
+	static DataAccess sut=new DataAccess();
+	static TestDataAccess testDA=new TestDataAccess();
+	
+	
+	
+	
+	
+	@Test
+	public void test1() {
+		
+		
+		String izena = "Lur10";
+		String pasahitza ="pasahitza_1";
+		String type ="Driver";
+		String mail ="ander@gmail";
+		
+		sut.open();
+		User t= sut.register(izena, pasahitza, type, mail);
+		sut.close();
+		
+		
+		try {
+			sut.open();
+			User u= sut.register(izena, pasahitza, type, mail);
+			sut.close();
+			
+			if(u != null ) {
+				
+				fail();
+			}
+			else {
+				assertTrue(true);
+			}
+			
+			
+			
+		}
+		finally {
+			sut.open();
+			sut.erabiltzaileaEzabatu(t);
+			sut.close();
+			
+			
+		}
+	}
+	
+	
+	
+	
+	@Test
+	public void test2() {
+		
+		String izena = "Lur";
+		String pasahitza ="pasahitza_1";
+		String type ="Driver";
+		String mail ="lur@gmail";
+		
+		
+		
+		
+		try {
+			sut.open();
+			User t= sut.register(izena, pasahitza, type, mail);
+			sut.close();
+			
+			if(t != null) {
+				assertTrue(true);
+				sut.open();
+				sut.erabiltzaileaEzabatu(t);
+				sut.close();
+			}
+			else {
+				fail();
+			}
+			
+			
+			
+		}
+		finally {
+			
+			
+		}
+	}
+	
+	
+	@Test
+	public void test3() {
+		String izena = "Lur";
+		String pasahitza ="pasahitza_1";
+		String type ="Traveler";
+		String mail ="lur@gmail";
+		
+		
+		
+		
+		
+		try {
+			sut.open();
+			User t= sut.register(izena, pasahitza, type, mail);
+			sut.close();
+			
+			if(t != null) {
+				sut.open();
+				sut.erabiltzaileaEzabatu(t);
+				sut.close();
+				assertTrue(true);
+				
+				
+			}
+			else {
+				fail();
+			}
+			
+			
+			
+		}
+		finally {
+		
+		}
+	}
+	
+	
+	
+	@Test
+	public void test4() {
+		String izena = "Lur";
+		String pasahitza ="pasahitza_1";
+		String type =null;
+		String mail ="lur@gmail";
+		
+		User u = new Traveler(izena,pasahitza,mail);
+		
+		
+		
+		try {
+			sut.open();
+			User t= sut.register(izena, pasahitza, type, mail);
+			sut.close();
+			
+			if(t != null) {
+				sut.open();
+				sut.erabiltzaileaEzabatu(u);
+				sut.close();
+				fail();
+			}
+			else {
+				
+				assertTrue(true);
+			}
+			
+			
+			
+		}
+		finally {
+			
+			
+		}
+			
+	}
+}
